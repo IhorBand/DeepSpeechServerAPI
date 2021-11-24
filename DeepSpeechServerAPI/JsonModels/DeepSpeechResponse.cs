e5d5d33e3cd272638792f0c9e2e68872f672fbf0
ihorb <igor.bandura.3@gmail.com>
2021-11-24 07:51:43 +0200

    Model for DeepSpeech Json Response

---
+++ DeepSpeechServerAPI/JsonModels/DeepSpeechResponse.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace DeepSpeechServerAPI.JsonModels
+{
+    public class DeepSpeechResponse
+    {
+        [JsonProperty("transcripts")]
+        public List<Transcript> Transcripts { get; set; }
+    }
+
+    public class Transcript
+    {
+        [JsonProperty("confidence")]
+        public string Confidence { get; set; }
+
+        [JsonProperty("words")]
+        public List<WordInfo> WordInfos { get; set; }
+    }
+
+    public class WordInfo
+    {
+        [JsonProperty("word")]
+        public string Word { get; set; }
+        [JsonProperty("start_time")]
+        public string StartTime { get; set; }
+        [JsonProperty("duration")]
+        public string Duration { get; set; }
+    }
+}
